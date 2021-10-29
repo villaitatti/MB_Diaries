c2355c6fd3ea9d043c95396de42ea862e156090b
gspinaci <gianmarco.spinaci@gmail.com>
2021-10-29 18:09:00 +0200

    NER results
    Signed-off-by: gspinaci <gianmarco.spinaci@gmail.com>

--- script.py
+++ script.py
@@ -3,19 +3,21 @@ import sys
 import os
 import re  
 import spacy
+import pandas as pd
 
 sys.path.append('assets')
 from convert import convert2text, convert2xml, convert2vec
 
 nlp = spacy.load('en_core_web_sm')
 
-def write_txt(filename, body):
+def write_file(filename, body):
   with open(filename, 'w') as f:
           f.write(body)
           f.close()
 
-def write_ner(filename, body):
-  return
+def write_csv(filename, body):
+  df = pd.DataFrame(body, columns=['paragraph', 'text', 'min', 'max', 'label', 'description'])
+  df.to_csv(filename, index=False)
 
 def parse_pages(pages):
   regexp = r'\[[0-9]+\][\s]*'
@@ -26,26 +28,24 @@ def parse_pages(pages):
     match = re.match(regexp, page)
     if match:
       if name is not None:
-        write_txt(os.path.join(output_path, 'txt', name), body)
-        write_ner(execute_ner(body))
+        write_file(os.path.join(output_path, 'txt', f'{name}.txt'), body)
+        write_csv(os.path.join(output_path, 'csv', f'{name}.csv'), execute_ner(body))
 
-      name = re.sub(r'[\[\]\s]', '', f'{match[0]}.txt')
+      name = re.sub(r'[\[\]\s]', '', match[0])
       body = f'{re.sub(regexp, "", page)}\n'
 
     else:
       body += f'{page}\n'
     
 def execute_ner(document):
-
-
-
-  def show_ents(doc):
-    if doc.ents:
-      for ent in doc.ents:
-        print(f'{ent.text} - {ent.start_char} - {ent.end_char} - {ent.label_} - {spacy.explain(ent.label_)}')
-
-  doc1 = nlp('Iris, Mr. and Mrs.  Curtis of Boston and Cecil lunched here. Miss Stanton and Count and Countess Rasponi came to tea. Miss Lowenthal, professor of Economics at Smith College came to spend the week on.')
-  show_ents(doc1)
+  data = []
+  pages = document.split('\n')
+  for idx, page in enumerate(pages):
+    doc = nlp(page)
+    for ent in doc.ents:
+      data.append([idx+1, ent.text, ent.start_char, ent.end_char, ent.label_, spacy.explain(ent.label_)])
+  
+  return data
 
 filename = '1930 DONE Mary Berenson DIary 1930-1931 (People coming to I Tatti).docx'
 input_path = os.path.join(os.getcwd(), 'assets', 'input')
