3d22e857b8089154e963a8f2f7f5473bff51e8e5
gspinaci <gianmarco.spinaci@gmail.com>
2021-11-02 14:17:44 +0100

    update NER with higher precision
    Signed-off-by: gspinaci <gianmarco.spinaci@gmail.com>

--- script.py
+++ script.py
@@ -1,23 +1,27 @@
 
 import sys
 import os
-import re  
+import re
 import spacy
 import pandas as pd
 
-sys.path.append('assets')
+sys.path.append('/Users/gspinaci/projects/diaries/MB_Diaries/assets')
 from convert import convert2text, convert2xml, convert2vec
 
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_trf')
+
 
 def write_file(filename, body):
   with open(filename, 'w') as f:
-          f.write(body)
-          f.close()
+    f.write(body)
+    f.close()
+
 
 def write_csv(filename, body):
-  df = pd.DataFrame(body, columns=['paragraph', 'text', 'min', 'max', 'label', 'description'])
+  df = pd.DataFrame(
+      body, columns=['text', 'p', 'start', 'end', 'type', 'description'])
   df.to_csv(filename, index=False)
+
 
 def parse_pages(pages):
   regexp = r'\[[0-9]+\][\s]*'
@@ -29,29 +33,33 @@ def parse_pages(pages):
     if match:
       if name is not None:
         write_file(os.path.join(output_path, 'txt', f'{name}.txt'), body)
-        write_csv(os.path.join(output_path, 'csv', f'{name}.csv'), execute_ner(body))
+        write_csv(os.path.join(output_path, 'csv',
+                  f'{name}.csv'), execute_ner(body))
 
       name = re.sub(r'[\[\]\s]', '', match[0])
       body = f'{re.sub(regexp, "", page)}\n'
 
     else:
       body += f'{page}\n'
-    
+
+
 def execute_ner(document):
   data = []
   pages = document.split('\n')
   for idx, page in enumerate(pages):
     doc = nlp(page)
     for ent in doc.ents:
-      data.append([idx+1, ent.text, ent.start_char, ent.end_char, ent.label_, spacy.explain(ent.label_)])
-  
+      data.append([ent.text, idx+1, ent.start_char, ent.end_char,
+                  ent.label_, spacy.explain(ent.label_)])
+
   return data
 
+
 filename = '1930 DONE Mary Berenson DIary 1930-1931 (People coming to I Tatti).docx'
-input_path = os.path.join(os.getcwd(), 'assets', 'input')
-output_path = os.path.join(os.getcwd(), 'assets', 'output')
+cur_path = os.path.dirname(os.path.realpath(__file__))
+input_path = os.path.join(cur_path, 'assets', 'input')
+output_path = os.path.join(cur_path, 'assets', 'output')
 file_input = os.path.join(input_path, filename)
 
 vec = convert2vec(file_input)
 parse_pages(vec)
-
